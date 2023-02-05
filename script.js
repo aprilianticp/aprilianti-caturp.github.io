60347f0628a5ff596d8a70e92adafd11308b4aa7
aprilianticp <114726249+aprilianticp@users.noreply.github.com>
2023-02-05 23:08:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-// JavaScript
-const navigation = document.querySelector("nav");
-const header = document.querySelector("header");
+// Membuat variabel untuk menyimpan elemen HTML
+var header = document.querySelector("header");
+var navLinks = document.querySelectorAll("header nav a");
+var sections = document.querySelectorAll("main section");
 
+// Menambahkan class active pada link yang sedang dikunjungi
+navLinks.forEach(function(link) {
+  link.addEventListener("click", function() {
+    // Menghapus class active pada semua link
+    navLinks.forEach(function(navLink) {
+      navLink.classList.remove("active");
+    });
+
+    // Menambahkan class active pada link yang sedang diklik
+    link.classList.add("active");
+
+    // Mendapatkan seksi yang sesuai dengan link yang diklik
+    var sectionId = link.getAttribute("href");
+    var section = document.querySelector(sectionId);
+
+    // Menampilkan seksi yang sesuai
+    sections.forEach(function(section) {
+      section.style.display = "none";
+    });
+    section.style.display = "block";
+  });
+});
+
+// Menambahkan efek scroll pada header
 window.addEventListener("scroll", function() {
-  if (window.pageYOffset > header.offsetHeight) {
-    navigation.classList.add("sticky");
+  var scrollPos = window.scrollY;
+  if (scrollPos > 20) {
+    header.classList.add("scrolled");
   } else {
-    navigation.classList.remove("sticky");
+    header.classList.remove("scrolled");
   }
 });
